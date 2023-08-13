d2822491fbac31f230580f5fea015ea32f3fd9cb
DumbestDev <juunoda@gmail.com>
2023-08-12 22:30:25 -0300

    another command fix

--- lala/index.js
+++ lala/index.js
@@ -14,4 +14,22 @@ const intents = [
 const client = new Client({ intents, fetchAllMembers: true });
 client.login(process.env.BOT_TOKEN);
 
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+            errorHandler(`Command (${filePath}) is missing properties. (definition/implementation)`, "loading commands");
+		}
+	}
+}
+
 readEvents(client);
